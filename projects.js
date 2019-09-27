eab34876e026e5302ed5f42f7c198e199ec6d055
David Wachlin <davidwachlin@gmail.com>
2019-09-27 16:49:34 -0400

    99 bottles: console function and with dom inputs completed

--- projects.js
+++ projects.js
@@ -4,3 +4,24 @@
 // - Besides the phrase "take one down," you may not type in any numbers/names of numbers directly into your song lyrics.
 // - Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
 
+function sing99Bottles(num) {
+    for (let i = num; i >= 0; i--) {
+        if (i > 1) {
+            console.log(`${i} bottles of beer on the wall, ${i} bottles of beer, you take one down, you pass it around, ${i} bottles of beer on the wall`);
+        } else if (i === 1) {
+            console.log(`${i} bottle of beer on the wall, ${i} bottle of beer, you take it down, you pass it around, ${i} bottle of beer on the wall`);
+        } else if (i === 0) {
+            num = "No"
+            console.log(`${num} bottles of beer on the wall, ${num} bottles of beer, you take one down, you pass it around, ${num} bottles of beer on the wall`);
+            return;
+        }
+    }
+}
+
+// let inputNumber = document.getElementById('inputNum').value;
+let singButton = document.getElementById('singButton');
+
+singButton.addEventListener('click', () => {
+    let inputNumber = document.getElementById('inputNum').value;
+    sing99Bottles(inputNumber)
+});
